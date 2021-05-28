616103b4da0f3d8fb9ec0c9fd8d82eb4102c1756
eternalmgy01 <60344310+eternalmgy01@users.noreply.github.com>
2021-05-28 08:04:31 +0300

    mobileFinal

---
+++ lib/constants.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+// Colors that we use in our app
+const kPrimaryColor = Color(0xFFBBDEFB);
+const kTextColor = Color(0xFF263238);
+const kBackgroundColor = Color(0xFFECEFF1);
+
+const double kDefaultPadding = 20.0;
